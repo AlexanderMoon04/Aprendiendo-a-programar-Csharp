1a362d23e72d50b8483dd70aa2b554a36bfb49b0
Alejandro Luna <alexitrix@gmail.com>
2025-06-27 15:58:46 -0600

    Actualizaction trés sympa

---
+++ Fibonaccii/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Fibonaccii;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //1 2 3 4 5 6 7 8  9  10 11 12
+        //0 1 1 2 3 5 8 13 21 34 55 89
+        Console.WriteLine("Enter a number to get its Fibonacci value: ");
+        int number = int.Parse(Console.ReadLine());
+        System.Console.WriteLine("Fibonacci value: " + Fibonacci(number));
+        System.Console.WriteLine("Fibonacci value (Recursive edition): " + RecursiveFibonacci(number));
+    }
+
+    public static int Fibonacci(int x)
+    {
+        int f1 = 0, f2 = 1, f3 = 0;
+        if (x <= 0)
+        {
+            return -1; //Error for invalid input
+        }
+        for (int i = 2; i < x; i++)
+        {
+            f3 = f1 + f2; // Calculate the next Fibonacci number
+            f1 = f2; // Update f1 to the previous Fibonacci number
+            f2 = f3;    // Update f2 to the current Fibonacci number
+        }
+        return f3;
+
+    }
+
+    public static int RecursiveFibonacci(int x)
+    {
+        if (x <= 0)
+        {
+            return -1; //Error for invalid input
+        }
+        if (x == 1)
+        {
+            return 0;
+        }
+        if (x == 2)
+        {
+            return 1;
+        }
+        return RecursiveFibonacci(x - 1) + RecursiveFibonacci(x - 2); // Recursive call to calculate Fibonacci number
+    } 
+}
---
+++ NumPares/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel;
+using System.Reflection.Metadata.Ecma335;
+
+namespace NumPares;
+
+class Program
+{
+    /* Crear una funcion que imprime numeros pares de 2 hasta n 
+    - En estructurado
+    -En recursivo
+     */
+    static void Main(string[] args)
+    {
+        int Numero = 0;
+        Console.Write("Salut, dame un numero: ");
+        Numero = int.Parse(Console.ReadLine());
+        Console.WriteLine(" Cantidad de numeros par: " + Pares(Numero));
+        Console.WriteLine(" Cantidad de numeros par: " + ParesRecursivo(Numero));
+    }
+
+    static public int Pares(int Numero)
+    {
+        int par = 0, numpar = 0;
+        for (int i = 2; i < Numero; i++)
+        {
+            if ((i % 2) == 0)
+            {
+                numpar += 2;
+                par++;
+                Console.Write(numpar + ",");
+            }
+        }
+        return par;
+    }
+
+    static public int ParesRecursivo(int par)
+    {
+        if (par < 2)
+        {
+            return 0;
+        }
+        if (par % 2 == 0 && par >= 2)
+            {
+                Console.Write(par + ",");
+                return ParesRecursivo(par - 2);
+            }
+            else
+                return ParesRecursivo(par - 1);
+    }
+}
